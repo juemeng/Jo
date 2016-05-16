6a2d0e0788e714886ada3c75d0f4c5f60d5ca691
Jo <zoul@shinetechchina.com>
2016-05-16 17:58:46 +0800

    finish

--- bin/jo.js
+++ bin/jo.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node 
+
 'use strict';
 
 var _path = require('path');
@@ -17,27 +18,56 @@ var _child_process = require('child_process');
 
 var _package = require('../package.json');
 
+var _rimraf = require('rimraf');
+
+var _rimraf2 = _interopRequireDefault(_rimraf);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var gitRepoUrl = 'https://github.com/juemeng/Jo.git';
-
-var log = console.log;
-var cmdValue = "";
-var envValue = "";
+var gitRepoUrl = "https://github.com/juemeng/Jo.git";
 
 _commander2.default.version(_package.version).command('init [dir]').action(function (dir) {
     if (dir) {
-        var ls = (0, _child_process.spawn)('git', ['clone', '--recursive', gitRepoUrl, dir], { 'shell': true });
-        ls.stdout.on('data', function (data) {});
-
-        ls.stderr.on('data', function (data) {
-            console.log('stderr: ' + data);
-        });
-
-        ls.on('close', function (code) {
-            console.log('child process exited with code ' + code);
-        });
-    } else {}
+        if (dir.indexOf('/') === -1 && dir.indexOf('\\') === -1) {
+            console.log('invalid target path, please input again');
+            return;
+        }
+        cloneStarterKit(dir);
+    } else {
+        cloneStarterKit();
+    }
 });
 
 _commander2.default.parse(process.argv);
+
+function cloneStarterKit(dir) {
+
+    var target = process.cwd();
+
+    if (dir) {
+        target = dir;
+    }
+
+    var gitArgs = ['clone', '--recursive', gitRepoUrl, target];
+
+    var ls = (0, _child_process.spawn)('git', gitArgs, { 'shell': true });
+
+    ls.stdout.on('data', function (data) {
+        console.log(data.toString());
+    });
+
+    ls.stderr.on('data', function (data) {
+        console.log(data.toString());
+    });
+
+    ls.on('close', function (code) {
+        if (code === 0) {
+            var gitPath = _path2.default.join(target, '.git');
+            (0, _rimraf2.default)(gitPath, function () {
+                console.log("init success");
+            });
+        } else {
+            console.log('child process exited with code ' + code);
+        }
+    });
+}
--- src/jo.js
+++ src/jo.js
@@ -1,38 +1,61 @@
 #!/usr/bin/env node 
+'use strict';
 
 import path from 'path';
 import fs from 'fs';
 import program from 'commander';
 import {spawn} from 'child_process';
 import {version} from '../package.json';
+import rimraf from 'rimraf';
 
-const gitRepoUrl = 'https://github.com/juemeng/Jo.git';
-
-
-const log = console.log;
-let cmdValue = "";
-let envValue = "";
+const gitRepoUrl = "https://github.com/juemeng/Jo.git";
 
 program
   .version(version)
   .command('init [dir]')
   .action(function (dir) {
      if(dir){
-         const ls = spawn('git', ['clone', '--recursive', gitRepoUrl, dir], { 'shell': true });
-         ls.stdout.on('data', (data) => {
-
-         });
-
-         ls.stderr.on('data', (data) => {
-             console.log(`stderr: ${data}`);
-         });
-
-         ls.on('close', (code) => {
-             console.log(`child process exited with code ${code}`);
-         });
+         if(dir.indexOf('/') === -1 && dir.indexOf('\\') === -1)
+         {
+             console.log('invalid target path, please input again');
+             return;
+         }
+         cloneStarterKit(dir);
      }else {
-         
+         cloneStarterKit();
      }
   });
 
 program.parse(process.argv);
+
+function cloneStarterKit(dir) {
+    
+    let target = process.cwd();
+
+    if(dir) {
+        target = dir;
+    }
+    
+    let gitArgs = ['clone', '--recursive', gitRepoUrl,target];
+    
+    const ls = spawn('git', gitArgs, { 'shell': true });
+
+    ls.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    ls.stderr.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    ls.on('close', (code) => {
+        if (code === 0) {
+            let gitPath = path.join(target,'.git');
+            rimraf(gitPath, function () {
+                console.log("init success");
+            });
+        } else {
+            console.log(`child process exited with code ${code}`);
+        }
+    });
+}
